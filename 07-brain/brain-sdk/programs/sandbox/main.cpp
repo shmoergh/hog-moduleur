69f9ce38512ffb6c22ff68a6cb0a8c1f0beb6b58
Peter Zimon <peter.zimon@gmail.com>
2025-09-05 07:19:51 +0200

    Added a task to debug launch to trigger a build

--- 07-brain/brain-sdk/programs/sandbox/main.cpp
+++ 07-brain/brain-sdk/programs/sandbox/main.cpp
@@ -1,16 +1,20 @@
+#include <pico/stdio.h>
 #include <pico/stdlib.h>
+
+#include <iostream>
 
 const uint LED_PIN = PICO_DEFAULT_LED_PIN;
 
 int main() {
-	stdio_init_all();
-	gpio_init(LED_PIN);
-	gpio_set_dir(LED_PIN, GPIO_OUT);
-	while (true) {
-		gpio_put(LED_PIN, 1);
-		sleep_ms(500);
-		gpio_put(LED_PIN, 0);
-		sleep_ms(500);
-	}
-	return 0;
+    stdio_init_all();
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    while (true) {
+        gpio_put(LED_PIN, 1);
+        sleep_ms(100);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(100);
+        printf("Halo now again\n\r");
+    }
+    return 0;
 }
