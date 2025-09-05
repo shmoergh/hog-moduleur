a0cf506a35ab38b5bb063bc9366bc3e1ed543794
Peter Zimon <peter.zimon@gmail.com>
2025-09-05 07:07:59 +0200

    Updated new program script format

--- 07-brain/brain-sdk/programs/blinky/main.cpp
+++ 07-brain/brain-sdk/programs/blinky/main.cpp
@@ -6,6 +6,7 @@ int main() {
     stdio_init_all();
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
+
     while (true) {
         gpio_put(LED_PIN, 1);
         sleep_ms(500);
--- 07-brain/brain-sdk/scripts/new-program.sh
+++ 07-brain/brain-sdk/scripts/new-program.sh
@@ -35,16 +35,16 @@ cat > "$PROG_DIR/main.cpp" <<EOF
 const uint LED_PIN = PICO_DEFAULT_LED_PIN;
 
 int main() {
-  stdio_init_all();
-  gpio_init(LED_PIN);
-  gpio_set_dir(LED_PIN, GPIO_OUT);
-  while (true) {
-    gpio_put(LED_PIN, 1);
-    sleep_ms(500);
-    gpio_put(LED_PIN, 0);
-    sleep_ms(500);
-  }
-  return 0;
+	stdio_init_all();
+	gpio_init(LED_PIN);
+	gpio_set_dir(LED_PIN, GPIO_OUT);
+	while (true) {
+		gpio_put(LED_PIN, 1);
+		sleep_ms(500);
+		gpio_put(LED_PIN, 0);
+		sleep_ms(500);
+	}
+	return 0;
 }
 EOF
 
